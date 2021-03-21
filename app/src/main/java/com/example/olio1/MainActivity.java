d93d96cceedf4bc257259dd89601ed198f7397e4
valitti <piilolavaltti@gmail.com>
2021-03-21 17:02:17 +0200

    Teht1

---
+++ app/src/main/java/com/example/olio1/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.olio1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void painaNappia (View v) {
+        System.out.println("Hello World!");
+    }
+}
