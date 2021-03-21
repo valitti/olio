f8bae2d9b20538d923eefa7f34fb548dd289f524
valitti <piilolavaltti@gmail.com>
2021-03-21 17:23:26 +0200

    first change

--- app/src/main/java/com/example/olio1/MainActivity.java
+++ app/src/main/java/com/example/olio1/MainActivity.java
@@ -15,5 +15,6 @@ public class MainActivity extends AppCompatActivity {
 
     public void painaNappia (View v) {
         System.out.println("Hello World!");
+        System.out.println("hello hello");
     }
 }
