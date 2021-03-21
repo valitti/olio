860011c6e708701fba4d8cb9586f0b67d882128a
valitti <piilolavaltti@gmail.com>
2021-03-21 23:53:24 +0200

    Teht5

--- app/src/main/java/com/example/olio1/MainActivity.java
+++ app/src/main/java/com/example/olio1/MainActivity.java
@@ -1,31 +1,67 @@
 package com.example.olio1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
-import android.text.Editable;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.text.TextWatcher;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView text;
-    EditText newText;
+    EditText writeName, readName, input;
+
+    Context context = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        text = findViewById(R.id.textView2);
-        newText = findViewById(R.id.plain_text_input);
-        newText.addTextChangedListener(new TextWatcher() {
-            public void afterTextChanged(Editable s) {
-                String input = newText.getText().toString();
-                text.setText(input);
-            }
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
-            public void onTextChanged(CharSequence s, int start, int after, int count) {}
-        });
+        input = findViewById(R.id.fileInput);
+        writeName = findViewById(R.id.writeName);
+        readName = findViewById(R.id.readName);
+        context = MainActivity.this;
+    }
 
+    public void writeFile(View v) {
+        try{
+            String name, text;
+            name = writeName.getText().toString();
+            text = input.getText().toString();
+            OutputStreamWriter writer = new OutputStreamWriter(context.openFileOutput(name, Context.MODE_PRIVATE));
+            writer.write(text);
+            writer.close();
+        } catch (IOException e) {
+            Log.e("IOException", "Virhe");
+        } finally {
+            System.out.println("Done");
+        }
+    }
+
+    public void readFile(View v) {
+        try{
+            String name, line;
+            input.setText("");
+            name = readName.getText().toString();
+            InputStream is = context.openFileInput(name);
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+
+            while ((line = br.readLine()) != null) {
+                line = (line + System.getProperty("line.separator"));
+                input.append(line);
+            }
+            is.close();
+        } catch (IOException e) {
+            Log.e("IOException", "Virhe");
+        } finally {
+            System.out.println("Done");
+        }
     }
 }
