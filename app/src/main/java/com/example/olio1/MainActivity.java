f239c3e2540c71ffcea3dea2e3d1a53f94c79e7c
valitti <piilolavaltti@gmail.com>
2021-03-21 20:29:25 +0200

    Teht4

--- app/src/main/java/com/example/olio1/MainActivity.java
+++ app/src/main/java/com/example/olio1/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.olio1;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.text.TextWatcher;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,9 +18,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         text = findViewById(R.id.textView2);
         newText = findViewById(R.id.plain_text_input);
-    }
+        newText.addTextChangedListener(new TextWatcher() {
+            public void afterTextChanged(Editable s) {
+                String input = newText.getText().toString();
+                text.setText(input);
+            }
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            public void onTextChanged(CharSequence s, int start, int after, int count) {}
+        });
 
-    public void buttonPressed (View v) {
-        text.setText(newText.getText().toString());
     }
 }
