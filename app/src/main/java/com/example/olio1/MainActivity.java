6193bc6f9955b01289086d49a4090f5bbbfb4080
valitti <piilolavaltti@gmail.com>
2021-03-21 19:07:12 +0200

    Teht2

--- app/src/main/java/com/example/olio1/MainActivity.java
+++ app/src/main/java/com/example/olio1/MainActivity.java
@@ -1,20 +1,24 @@
 package com.example.olio1;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView text;
+    EditText newText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        text = findViewById(R.id.textView2);
+        newText = findViewById(R.id.plain_text_input);
     }
 
-    public void painaNappia (View v) {
-        System.out.println("Hello World!");
-        System.out.println("hello hello");
+    public void buttonPressed (View v) {
+        text.setText(newText.getText().toString());
     }
 }
